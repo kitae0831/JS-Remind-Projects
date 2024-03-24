79080066c98b76248074eb100fbabaad6cd161b1
kitaePark <bkt4044@naver.com>
2024-03-24 23:46:05 +0900

    Feat: Submit할 때 성공&실패 효과 주는 코드

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -5,7 +5,21 @@ const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
 // Show input error message
-function showError(input, message) {}
+function showError(input, message) {
+  const formControl = input.parentElemnet;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElemnet;
+  formControl.className = "form-control success";
+}
+
+// Check email is valid
+function isValidEmail(email) {}
 
 // Event listeners
 form.addEventListener("submit", function (e) {
@@ -15,5 +29,24 @@ form.addEventListener("submit", function (e) {
     showError(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email, value)) {
+    showError(email, "Email is required");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "Password  2 is required");
+  } else {
+    showSuccess(password2);
   }
 });
