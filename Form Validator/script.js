44ce68b0a40125ec694e377f9b9a7bc66eec705c
kitaePark <bkt4044@naver.com>
2024-03-26 23:29:05 +0900

    Feat: Email 정규식 추가

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -6,7 +6,7 @@ const password2 = document.getElementById("password2");
 
 // Show input error message
 function showError(input, message) {
-  const formControl = input.parentElemnet;
+  const formControl = input.parentElement;
   formControl.className = "form-control error";
   const small = formControl.querySelector("small");
   small.innerText = message;
@@ -14,12 +14,16 @@ function showError(input, message) {
 
 // Show success outline
 function showSuccess(input) {
-  const formControl = input.parentElemnet;
+  const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
 // Check email is valid
-function isValidEmail(email) {}
+function isValidEmail(input) {
+  const emailValue = input.value;
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(emailValue).toLowerCase());
+}
 
 // Event listeners
 form.addEventListener("submit", function (e) {
