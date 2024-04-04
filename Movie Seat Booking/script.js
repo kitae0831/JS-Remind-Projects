136093a25d153a9cf754ccdcc1911e212a6384d1
kitaePark <bkt4044@naver.com>
2024-04-05 00:51:19 +0900

    Feat: Select에 따른 movie 가격 차등화 구현

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -6,14 +6,33 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("occupied")
+    // 빈자리에만 작동하도록 조건을 부여!
   ) {
-    // e.target.classList.toggle("selected");
-    console.log(e.target);
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
   }
 });
