c69462a01e0b9a8796bb857d4aea228a15cca593
kitaePark <bkt4044@naver.com>
2024-04-14 23:54:20 +0900

    Feat: 버튼 기능 구현

--- Custom Video Player/script.js
+++ Custom Video Player/script.js
@@ -3,3 +3,40 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
+
+// Play & pause video
+function toggleVideoStatus() {
+  return true;
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  return true;
+}
+
+// Update Progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+  return true;
+}
+
+// Event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("time", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
