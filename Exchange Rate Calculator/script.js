2983bb4d7f1796dafc1ca48bf1d4db2178c4eb9f
kitaePark <bkt4044@naver.com>
2024-05-05 21:17:38 +0900

    Feat: fetch 대신 json 사용

--- Exchange Rate Calculator/script.js
+++ Exchange Rate Calculator/script.js
@@ -1,9 +1,12 @@
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
 function caclulate() {
   fetch("item.json")
     .then((res) => res.json())
     .then((data) => (document.body.innerHTML = data[0].text));
 }
-
-console.log;
 
 caclulate();
