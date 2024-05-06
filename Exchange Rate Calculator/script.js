03a2e30945ee1b28cc739193743b12a27217e12d
kitaePark <bkt4044@naver.com>
2024-05-06 21:44:40 +0900

    Feat: fetch -> json

--- Exchange Rate Calculator/script.js
+++ Exchange Rate Calculator/script.js
@@ -3,10 +3,26 @@ const amountEl_one = document.getElementById("amount-one");
 const currencyEl_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
 
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+//Fetch exchange rates and update the DOM
 function caclulate() {
-  fetch("item.json")
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
     .then((res) => res.json())
-    .then((data) => (document.body.innerHTML = data[0].text));
+    .then((data) => {
+      const rate = data.rates[currencyEl_two];
+
+      rateEl.innerTexst = `1 ${currencyEl_one} = ${rate} ${currencyEl_two}`;
+    });
 }
+
+// Event listners
+currencyEl_one.addEventListener("change", caclulate);
+amountEl_one.addEventListener("input", caclulate);
+currencyEl_two.addEventListener("change", caclulate);
+amountEl_two.addEventListener("input", caclulate);
 
 caclulate();
