4b8557b7034cb88638f4343139f83308d152479b
kitaePark <bkt4044@naver.com>
2024-05-17 00:15:34 +0900

    Feat: for문 -> forEach

--- DOM Array/script.js
+++ DOM Array/script.js
@@ -38,5 +38,9 @@ function updateDOM(providedData = data) {
   // Clear main div
   main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
 
-  providedData.forEach();
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+  });
 }
