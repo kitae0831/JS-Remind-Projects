359eead48ada2b9acdc29bc03b8ef5eda5c7303a
kitaePark <bkt4044@naver.com>
2024-05-18 02:12:37 +0900

    Feat: update

--- DOM Array/script.js
+++ DOM Array/script.js
@@ -26,6 +26,15 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+// Double everyones money
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
