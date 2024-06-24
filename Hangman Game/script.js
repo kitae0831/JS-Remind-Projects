c009b8149330a3e0641afbd4c88108b47769e213
kitaePark <bkt4044@naver.com>
2024-06-24 22:58:19 +0900

    Feat: 셋팅 완료

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -6,3 +6,10 @@ const notification = document.getElementById("notification-container");
 const fianlMessage = document.getElementById("fianl-message");
 
 const figureParts = document.querySelectorAll(".figrue-part");
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
