3f55271336148fbd505b40189f26a3be4b36fafc
kitaePark <bkt4044@naver.com>
2024-07-13 22:09:20 +0900

    Feat: API ㅌ토ㅗㅇ신

--- Meal Finder/script.js
+++ Meal Finder/script.js
@@ -5,4 +5,30 @@ const search = document.getElementById("search"),
   resultHeading = document.getElementById("result-heading"),
   single_mealEl = document.getElementById("single-meal");
 
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get Search term
+  const term = search.value;
+
+  // Check for empty
+  if (term.trim()) {
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1search.php?s=${term}
+      `
+        .then((res) => res.json())
+        .then((dat) => {
+          console.log(data);
+        })
+    );
+  } else {
+    alert("Please enter a search test");
+  }
+}
+
 // Event listeners
+submit.addEventListener("submit", searchMeal);
