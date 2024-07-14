10743726b23c4f0ffee03766bc7152b5cc5ad59b
kitaePark <bkt4044@naver.com>
2024-07-14 23:41:24 +0900

    Feat: API & 조건문

--- Meal Finder/script.js
+++ Meal Finder/script.js
@@ -21,8 +21,26 @@ function searchMeal(e) {
       `https://www.themealdb.com/api/json/v1/1search.php?s=${term}
       `
         .then((res) => res.json())
-        .then((dat) => {
+        .then((data) => {
           console.log(data);
+          resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+          if (data.meals === null) {
+            resultHeading.innerHTML = `<p>There are no search results. Try again</p>`;
+          } else {
+            mealsEl.innerHTML = data.meals
+              .map(
+                (meal) => `
+              <div class="meal">
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="meal-info" data=mealID="${meal.idMeal}">
+                  <h3>${meal.strMeal}</h3>
+                </div>
+              </div>
+              `
+              )
+              .join("");
+          }
         })
     );
   } else {
