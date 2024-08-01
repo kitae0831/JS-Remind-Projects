6f3025546ccf1f37db770a8870b22d0fac333765
kitaePark <bkt4044@naver.com>
2024-08-01 12:49:47 +0900

    Feat: DOM list 기능

--- Expense Track/script.js
+++ Expense Track/script.js
@@ -12,3 +12,24 @@ const dummyTransactions = [
   { id: 3, text: "Book", amount: -10 },
   { id: 4, text: "Camera", amount: 150 },
 ];
+
+let transactions = dummyTransactions;
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  // Get sign
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const item = document.createElement("li");
+
+  // Add class based on value
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn">x</button>
+  `;
+
+  list.appendChild(tiem);
+}
