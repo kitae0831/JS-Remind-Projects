4ee5ae4858dcac6426f285b66b1e83bf7e6f7991
kitaePark <bkt4044@naver.com>
2024-08-05 23:31:30 +0900

    Feat: 수입 수출 항목

--- Expense Track/script.js
+++ Expense Track/script.js
@@ -38,7 +38,17 @@ function addTransactionDOM(transaction) {
 function updateValues() {
   const amounts = transactions.map((transaction) => transaction.amount);
 
-  console.log(amounts);
+  const total = amount.reduce((acc, item) => (acc += item)).toFixed(2);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, itme) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
 }
 
 // Init app
