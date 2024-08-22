2334b39713ddfdd16db6b78eca886941ae42ebed
kitaePark <bkt4044@naver.com>
2024-08-22 23:38:38 +0900

    Feat: JS

---
+++ Music Player/script.js
@@ -0,0 +1,16 @@
+const musicContainer = document.getElementById("music_container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// Song titles
+const songs = ["hey", "summer", "ukulele"];
+
+// Keep track of song
+let songIndex = 2;
