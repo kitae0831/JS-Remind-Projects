6fdcb97f4366a0e79b6016654f87883638046b88
kitaePark <bkt4044@naver.com>
2024-09-11 18:01:26 +0900

    Feat: Show post

--- Infinity Scroll/script.js
+++ Infinity Scroll/script.js
@@ -14,3 +14,23 @@ async function getPosts() {
 
   return data;
 }
+
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
+    <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+    </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+showPosts();
